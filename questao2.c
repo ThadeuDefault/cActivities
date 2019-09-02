ff75e5f54f7344312570bf138ec5dbfad8401f47
Thadeu Default <thadeudefault@gmail.com>
2019-09-02 11:57:22 -0300

    Add files via upload

---
+++ questao2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int n;
+
+    scanf("%d",&n);
+
+    if (n%2 == 0){
+
+        printf("O numero %d e par", &n);
+
+    }else{
+
+        printf("O numero %d e impar", n);
+
+    }
+
+}
