37f0eb603d619664e0553b8eecf5c996fb24cc19
Thadeu Default <thadeudefault@gmail.com>
2019-09-04 16:20:32 -0300

    Add files via upload

---
+++ Atividades/questao9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    unsigned long int fat = 1;
+
+    scanf("%d", &n);
+    printf("\n");
+
+    int i = n;
+
+    for(i; i > 1; i--){
+        printf("%d\n", i);
+        printf("*\n");
+     fat = fat * i;
+
+    }
+
+        printf("1\n");
+        printf("\nFatorial de %d e igual a: %d", n, fat );
+
+
+
+
+
+}
